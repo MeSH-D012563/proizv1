ca43618877d08e0778fffde6d954cfc5a31ef286
Igor <IgorStrizhakov@yandex.ru>
2025-02-11 17:20:02 +0300

    random circle

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 import sys
 import random
-from PyQt6 import QtWidgets, uic
-from PyQt6.QtGui import QPainter, QColor, QBrush
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6 import QtWidgets, QtGui, QtCore
 
 
-class MainWindow(QMainWindow):
+class Ui_MainWindow:
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(400, 300)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(150, 120, 100, 30))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.setText("Draw Circle")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 400, 24))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.button = self.findChild(QtWidgets.QPushButton, 'pushButton')
-        self.button.clicked.connect(self.draw_circle)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.draw_circle)
         self.circles = []
 
     def draw_circle(self):
         diameter = random.randint(10, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+        color = QtGui.QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.circles.append((x, y, diameter, color))
         self.update()
 
     def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setBrush(QBrush(QColor('yellow')))
-        for x, y, diameter in self.circles:
+        painter = QtGui.QPainter(self)
+        for x, y, diameter, color in self.circles:
+            painter.setBrush(QtGui.QBrush(color))
             painter.drawEllipse(x, y, diameter, diameter)
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
